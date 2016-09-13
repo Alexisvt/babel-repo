cb37b69f01d46cb3f54b0aa1070a75a244bad9db
Alexis <alexisvt@gmail.com>
2016-09-13 12:59:57 -0600

    inital commit

---
+++ babelconfigurationsample/build/bundle.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var greeting = 'Hi';
+console.log('-- ' + greeting + ' David');
+'use strict';
+
+var greeting = 'Hi';
+console.log('-- ' + greeting + ' Alexis');
+
+//# sourceMappingURL=bundle.js.map

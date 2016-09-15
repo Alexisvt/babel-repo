4d5b9ff554d494b828439465dde4b71e1fbaf458
Alexis <alexisvt@gmail.com>
2016-09-14 18:05:21 -0600

    cleaning js files add the ignore rule

--- babel-typescript/src/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const React = require('react');
-const ReactDOM = require('react-dom');
-const react_redux_1 = require('react-redux');
-const container_component_1 = require('./components/container-component');
-const store_1 = require('./store');
-ReactDOM.render(React.createElement(react_redux_1.Provider, {store: store_1.default}, 
-    React.createElement(container_component_1.default, null)
-), document.getElementById('main'));
